9f83e3f7bc65bb170bc7745cc441c6a7d09e697e
David Cristóbal <david@juanked.es>
2019-05-16 20:04:39 +0200

    Solucionados problemas con profesor

--- AsociacionCervecera.java
+++ AsociacionCervecera.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.ObjectInputStream.GetField;
+//import java.io.ObjectInputStream.GetField;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -19,7 +19,6 @@ public class AsociacionCervecera {
 
 	public static void main(String[] args) {
 		AsociacionCervecera ac = new AsociacionCervecera();
-		ac.DBconnect();
 		ac.mainMenu();
 		ac.DBclose();
 	}
@@ -32,22 +31,26 @@ public class AsociacionCervecera {
 		String pass = "bdupm";
 		String url = "jdbc:mysql://" + serverAddress + "/" + db;
 		try {
-			Class.forName(drv);
-			System.out.println("Procediendo a conexión");
-			conn = DriverManager.getConnection(url, user, pass);
-			conn.setAutoCommit(true);
-			return true;
+			System.out.println(conn);
+			if (conn==null) {
+				Class.forName(drv);
+				System.out.println("Procediendo a conexión");
+				conn = DriverManager.getConnection(url, user, pass);
+				conn.setAutoCommit(true);
+			}
 		} catch (Exception excepcion) {
 			System.out.println("Ha habido un problema con la conexión");
 			excepcion.printStackTrace();
 			return false;
 		}
+		return true;
 	}
 
 	public boolean DBclose() {
 		if (conn != null) {
 			try {
 				conn.close();
+				conn = null;
 			} catch (SQLException e) {
 				System.out.println("Ha habido un problema con la desconexión:");
 				e.printStackTrace();
@@ -61,37 +64,36 @@ public class AsociacionCervecera {
 	}
 
 	public boolean createTableEmpleado() {
-		String sql = "CREATE TABLE empleado" + "(ID_empleado INTEGER not NULL,"
-				+ "nombre VARCHAR(50)," + "direccion VARCHAR(100),"
-				+ "telefono VARCHAR(15)," + "salario DOUBLE,"
-				+ "ID_bar INTEGER not NULL," + "PRIMARY KEY (ID_empleado),"
-				+ "FOREIGN KEY(ID_bar) REFERENCES bar(ID_bar));";
+		String sql = "CREATE TABLE empleado" + "(ID_empleado INTEGER not NULL," + "nombre VARCHAR(50),"
+				+ "direccion VARCHAR(100)," + "telefono VARCHAR(15)," + "salario DOUBLE," + "ID_bar INTEGER not NULL,"
+				+ "PRIMARY KEY (ID_empleado)," + "FOREIGN KEY(ID_bar) REFERENCES bar(ID_bar));";
 		try {
+			DBconnect();
 			System.out.println("Creando tabla Empleado PATATA");
 			st = conn.createStatement();
 			st.executeUpdate(sql);
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.out.println("No se ha podido crear la tabla empleado");
+			System.err.println("No se ha podido crear la tabla empleado");
 			return false;
 		}
 		return true;
 	}
 
 	public boolean createTableGusta() {
-		String sql = "CREATE TABLE gusta" + "(ID_socio INTEGER not NULL,"
-				+ "ID_cerveza INTEGER not NULL," + "ID_bar INTEGER not NULL,"
-				+ "PRIMARY KEY (ID_socio, ID_cerveza, ID_bar),"
+		String sql = "CREATE TABLE gusta" + "(ID_socio INTEGER not NULL," + "ID_cerveza INTEGER not NULL,"
+				+ "ID_bar INTEGER not NULL," + "PRIMARY KEY (ID_socio, ID_cerveza, ID_bar),"
 				+ "FOREIGN KEY(ID_socio) REFERENCES socio(ID_socio),"
 				+ "FOREIGN KEY(ID_cerveza) REFERENCES cerveza(ID_cerveza),"
 				+ "FOREIGN KEY(ID_bar) REFERENCES bar(ID_bar));";
 		try {
+			DBconnect();
 			System.out.println("Creando tabla Gusta");
 			st = conn.createStatement();
 			st.executeUpdate(sql);
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.out.println("No se ha podido crear la tabla gusta");
+			System.err.println("No se ha podido crear la tabla gusta");
 			return false;
 		}
 		return true;
@@ -100,18 +102,16 @@ public class AsociacionCervecera {
 	public boolean loadEmpleados() {
 		String query = "INSERT INTO empleado(ID_empleado,nombre,direccion,telefono,salario,ID_bar) VALUES (?,?,?,?,?,?)";
 		int id[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		String nombre[] = { "Carmen Matin", "Ana Ruiz", "Mario Moreno",
-				"Laura Romero", "Luis Ruiz", "Benito Gil", "Dolores Molina",
-				"Julio Garrido", "Pilar Romero" };
-		String direccion[] = { "C/Sol,1", "C/Luna,2", "C/Estrella,3",
-				"C/Mercurio,4", "C/Venus,5", "C/Marte,6", "C/Jupiter,7",
-				"C/Jupiter,7", "C/Saturno,8" };
-		int telefono[] = { 699999999, 699999988, 699999977, 699999966,
-				699999955, 699999944, 699999933, 699999922, 699999911 };
-		float salario[] = { 1600, 1300, 1200, 1450, 1350, 1500, 1350, 1350,
-				1650 };
+		String nombre[] = { "Carmen Matin", "Ana Ruiz", "Mario Moreno", "Laura Romero", "Luis Ruiz", "Benito Gil",
+				"Dolores Molina", "Julio Garrido", "Pilar Romero" };
+		String direccion[] = { "C/Sol,1", "C/Luna,2", "C/Estrella,3", "C/Mercurio,4", "C/Venus,5", "C/Marte,6",
+				"C/Jupiter,7", "C/Jupiter,7", "C/Saturno,8" };
+		int telefono[] = { 699999999, 699999988, 699999977, 699999966, 699999955, 699999944, 699999933, 699999922,
+				699999911 };
+		float salario[] = { 1600, 1300, 1200, 1450, 1350, 1500, 1350, 1350, 1650 };
 		int ID_bar[] = { 1, 2, 2, 3, 3, 3, 4, 4, 5 };
 		try {
+			DBconnect();
 			PreparedStatement st = conn.prepareStatement(query);
 			for (int i = 0; i < id.length; i++) {
 				st.setInt(1, id[i]);
@@ -134,6 +134,7 @@ public class AsociacionCervecera {
 		ArrayList<Gusto> gusto = readData(fileName);
 		String query = "INSERT INTO gusta(ID_socio, ID_cerveza, ID_bar) VALUES(?,?,?)";
 		try {
+			DBconnect();
 			PreparedStatement st = conn.prepareStatement(query);
 			for (int i = 0; i < gusto.size(); i++) {
 				st.setInt(1, gusto.get(i).idSocio);
@@ -143,8 +144,7 @@ public class AsociacionCervecera {
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.out
-					.println("Ha habido un problema con la inserccion de gusta");
+			System.out.println("Ha habido un problema con la inserccion de gusta");
 			return false;
 		}
 		return true;
@@ -154,6 +154,7 @@ public class AsociacionCervecera {
 		ArrayList<Bar> respuesta = new ArrayList<Bar>();
 		String query = "SELECT * FROM bar";
 		try {
+			DBconnect();
 			st = conn.createStatement();
 			ResultSet rs = st.executeQuery(query);
 			while (rs.next()) {
@@ -175,6 +176,7 @@ public class AsociacionCervecera {
 		ArrayList<Cerveza> cervezas = new ArrayList<Cerveza>();
 		Map<String, Integer> fabricante = new HashMap<String, Integer>();
 		try {
+			DBconnect();
 			st = conn.createStatement();
 			ResultSet rs = st.executeQuery(query);
 			while (rs.next()) {
@@ -185,8 +187,7 @@ public class AsociacionCervecera {
 			System.out.println("Escoja una opción");
 			System.out.println(fabricante.keySet());
 			String input = sc.nextLine();
-			String query2 = "SELECT * FROM cerveza WHERE ID_fabricante ="
-					+ fabricante.get(input);
+			String query2 = "SELECT * FROM cerveza WHERE ID_fabricante =" + fabricante.get(input);
 			st = conn.createStatement();
 			ResultSet rs2 = st.executeQuery(query2);
 
@@ -195,8 +196,7 @@ public class AsociacionCervecera {
 				String nombre2 = rs2.getString("nombre");
 				String caracteristicas = rs2.getString("caracteristicas");
 				int idFabricante = rs2.getInt("ID_fabricante");
-				cervezas.add(new Cerveza(idCerveza, nombre2, caracteristicas,
-						idFabricante));
+				cervezas.add(new Cerveza(idCerveza, nombre2, caracteristicas, idFabricante));
 			}
 
 		} catch (SQLException e) {
@@ -211,6 +211,7 @@ public class AsociacionCervecera {
 		int porcentaje;
 		String query = "SELECT COUNT(ID_cerveza) AS suma FROM cerveza;";
 		try {
+			DBconnect();
 			st = conn.createStatement();
 			ResultSet rs = st.executeQuery(query);
 			rs.next();
@@ -219,18 +220,16 @@ public class AsociacionCervecera {
 					+ "FROM  gusta GROUP BY ID_cerveza ORDER BY total DESC LIMIT " + porcentaje;
 			ResultSet rs2 = st.executeQuery(query2);
 			rs2.next();
-			String query3 = "SELECT * FROM cerveza WHERE ID_cerveza = "
-					+ rs2.getInt("ID_cerveza") + ";";
+			String query3 = "SELECT * FROM cerveza WHERE ID_cerveza = " + rs2.getInt("ID_cerveza") + ";";
 			ResultSet rs3 = st.executeQuery(query3);
-			while(rs3.next()){
+			while (rs3.next()) {
 				int idCerveza = rs3.getInt("ID_cerveza");
 				String nombre = rs3.getString("nombre");
 				String caracteristicas = rs3.getString("caracteristicas");
 				int idFabricante = rs3.getInt("ID_fabricante");
-				populares.add(new Cerveza(idCerveza, nombre, caracteristicas,
-						idFabricante));
+				populares.add(new Cerveza(idCerveza, nombre, caracteristicas, idFabricante));
 			}
-				
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			populares = null;
@@ -241,13 +240,13 @@ public class AsociacionCervecera {
 	public boolean addFotoColumn() {
 		String query = "ALTER TABLE empleado ADD COLUMN foto LONGBLOB";
 		try {
+			DBconnect();
 			PreparedStatement pst = conn.prepareStatement(query); // STATEMENT
 																	// OR
 																	// PREPAREDSTATEMENT
 			pst.executeUpdate();
 		} catch (SQLException e) {
-			System.out
-					.println("Ha habido un problema con la inserccion de la columna");
+			System.out.println("Ha habido un problema con la inserccion de la columna");
 			e.printStackTrace();
 			return false;
 		}
@@ -259,6 +258,7 @@ public class AsociacionCervecera {
 				+ " VALUES (?,?,?,?,?,?,?)"; // sentencia SQL que se
 		// ejecutará
 		try {
+			DBconnect();
 			PreparedStatement pst = conn.prepareStatement(query);
 			File file = new File("HomerSimpson.jpg");
 			FileInputStream fis = new FileInputStream(file);
@@ -273,20 +273,12 @@ public class AsociacionCervecera {
 			pst.setInt(6, 1);
 			pst.setBinaryStream(7, fis, (int) file.length());
 			pst.executeUpdate();
-			// if (resultado == 1) {
-			// System.out.println("Se ha insertado correctamente");
-			// correcto = true;
-			// } else
-			// System.out
-			// .println("Ha habido un problema con la inserccion de los datos");
 		} catch (SQLException e) {
-			System.out
-					.println("Ha habido un problema con la inserccion de los datos");
+			System.out.println("Ha habido un problema con la inserccion de los datos");
 			e.printStackTrace();
 			return false;
 		} catch (FileNotFoundException e) {
-			System.out
-					.println("El archivo indicado no existe o no se encuentra en esta dirección");
+			System.out.println("El archivo indicado no existe o no se encuentra en esta dirección");
 			e.printStackTrace();
 			return false;
 		}
@@ -295,12 +287,12 @@ public class AsociacionCervecera {
 
 	/*
 	 * The private section of the code starts here. It is convenient to take a
-	 * glance to the code, but it is STRICTLY FORBIDDEN to modify any part of
-	 * the code bellow these lines.
+	 * glance to the code, but it is STRICTLY FORBIDDEN to modify any part of the
+	 * code bellow these lines.
 	 * 
-	 * Aquí comienza la parte "privada" del código. Es conviniente echar un
-	 * vistazo al código, pero está ESTRÍCTICAMENTE PROHIBIDO modificar
-	 * cualquier parte del código bajo estas líneas.
+	 * Aquí comienza la parte "privada" del código. Es conviniente echar un vistazo
+	 * al código, pero está ESTRÍCTICAMENTE PROHIBIDO modificar cualquier parte del
+	 * código bajo estas líneas.
 	 */
 
 	private void mainMenu() {
@@ -310,15 +302,12 @@ public class AsociacionCervecera {
 		// Main menu loop
 		do {
 			System.out.println("Escoja una opción: ");
-			System.out
-					.println("  1) Crear las tablas \"empleado\" y \"gusta\".");
-			System.out
-					.println("  2) Cargar datos de los empleados y los gustos.");
+			System.out.println("  1) Crear las tablas \"empleado\" y \"gusta\".");
+			System.out.println("  2) Cargar datos de los empleados y los gustos.");
 			System.out.println("  3) Listar los bares almacenados.");
 			System.out.println("  4) Listar las cervezas de un fabricante.");
 			System.out.println("  5) Listar las cervezas más populares.");
-			System.out
-					.println("  6) Añadir columna de foto a la tabla \"empleado\".");
+			System.out.println("  6) Añadir columna de foto a la tabla \"empleado\".");
 			System.out.println("  7) Añadir un nuevo empleado con foto.");
 			System.out.println("  0) Salir de la aplicación.");
 
@@ -348,8 +337,7 @@ public class AsociacionCervecera {
 				createTableGusta();
 				break;
 			case '2':
-				System.out
-						.println("Cargando datos de la tabla \"empleado\"...");
+				System.out.println("Cargando datos de la tabla \"empleado\"...");
 				loadEmpleados();
 				System.out.println("Cargando datos de la tabla \"gusta\"...");
 				loadGustos("gustos.csv");
@@ -367,8 +355,7 @@ public class AsociacionCervecera {
 				System.out.println(c.cervezasToString(cervezas) + '\n');
 				break;
 			case '6':
-				System.out
-						.println("Añadiendo columa \"foto\" en la tabla \"empleado\"...");
+				System.out.println("Añadiendo columa \"foto\" en la tabla \"empleado\"...");
 				addFotoColumn();
 				break;
 			case '7':
@@ -395,8 +382,8 @@ public class AsociacionCervecera {
 
 			while (sc_file.hasNextLine()) {
 				String[] fields = sc_file.nextLine().split(";");
-				Gusto row = new Gusto(Integer.valueOf(fields[0]),
-						Integer.valueOf(fields[1]), Integer.valueOf(fields[2]));
+				Gusto row = new Gusto(Integer.valueOf(fields[0]), Integer.valueOf(fields[1]),
+						Integer.valueOf(fields[2]));
 				result.add(row);
 			}
 
@@ -452,8 +439,7 @@ public class AsociacionCervecera {
 			String result = "Listado de bares: \n";
 			if (bars != null)
 				for (Bar bar : bars) {
-					result = result + "  " + bar.idBar + " - " + bar.nombre
-							+ " - " + bar.direccion + "\n";
+					result = result + "  " + bar.idBar + " - " + bar.nombre + " - " + bar.direccion + "\n";
 				}
 			return result;
 		}
@@ -515,8 +501,7 @@ public class AsociacionCervecera {
 			String result = "Listado de cervezas: \n";
 			if (cervezas != null)
 				for (Cerveza c : cervezas) {
-					result = result + "  " + c.idCerveza + " - " + c.nombre
-							+ " - " + c.caracteristicas + " - "
+					result = result + "  " + c.idCerveza + " - " + c.nombre + " - " + c.caracteristicas + " - "
 							+ c.idFabricante + "\n";
 				}
 			return result;
